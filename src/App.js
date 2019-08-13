64d9ba2265a2a4e9336fbd560dc1a616f51ac347
Robertking007 <robertking007@live.com>
2019-08-13 17:52:57 -0400

    bump

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+
+import './App.css';
+import Person from './Person/Person';
+
+
+
+class App extends Component {
+  state = {
+    persons: [
+      { id : '01', name: 'Max', age: 28 },
+      { id : '02', name: 'Manu', age: 29 },
+      { id : '03', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice(); odl way
+    const persons = [...this.state.persons];// spread operator to populate new(copy) array
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons});
+  }
+
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    // populate inmutable obj
+    const person = {...this.state.persons[personIndex]}
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState( {persons: persons } )
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+     
+
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              key={person.id}
+              click = {() => this.deletePersonHandler(this)}
+              name={person.name}
+               age={person.age}
+               changed={(event) => this.nameChangedHandler(event, person.id)}
+               />
+          })}
+          
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+        }
+
+    const classes = [];
+
+    if(this.state.persons.length <=2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold');
+    }
+
+    return (
+    
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+     
+    );
+  }
+}
+export default App;
