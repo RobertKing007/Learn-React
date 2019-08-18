0bac75ab05357063296f5ebf86b522adf9e4c618
Robertking007 <robertking007@live.com>
2019-08-18 02:26:45 -0400

    refactored app, learned component lifecycle, React hooks, mainly useEffect

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+
+import classes from './Cockpit.css';
+
+const cockpit = ( props ) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect')
+    setTimeout(() => {
+      alert('SAved data');
+    }, 1000);
+    return () => console.log('[Cockpit.js] clean up 1st here')
+  }, []);
+
+  useEffect(()=>{
+    console.log('[Cockpit.js] 2ns useEffect')
+    return () => console.log('[Cockpit.js] clean up 2nd here')
+  }
+  );
+
+    const assignedClasses = [];
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if ( props.persons.length <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
+    }
+    if ( props.persons.length <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1>{props.title}</h1>
+            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+};
+
+export default cockpit;
